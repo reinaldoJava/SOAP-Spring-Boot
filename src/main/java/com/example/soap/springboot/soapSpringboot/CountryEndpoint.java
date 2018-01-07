5eaecd20f82eb345d9c5d0a238b8da6124f699ae
Reinaldo Santos <reinaldo.java@gmail.com>
2018-01-07 20:19:27 -0200

    commit inicial

---
+++ src/main/java/com/example/soap/springboot/soapSpringboot/CountryEndpoint.java
@@ -0,0 +1,88 @@
+package com.example.soap.springboot.soapSpringboot;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.SpanAccessor;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+	@Autowired
+	private Tracer tracer;
+
+	@Autowired
+	private SpanAccessor spanAccessor;
+
+	
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+//	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+//	@ResponsePayload
+//	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+//		GetCountryResponse response = new GetCountryResponse();
+//		response.setCountry(countryRepository.findCountry(request.getName()));
+//
+//		return response;
+//	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		try {
+			response.setCountry(countryRepository.findCountry(request.getName()));
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			Span span = tracer.getCurrentSpan();
+			//tracer.detach(span);
+			headers.add("traceId", span.traceIdString());
+			headers.add("spanId", Span.idToHex(span.getSpanId()));
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			
+			HttpEntity<String> requestRest = new HttpEntity<>("{}", headers);
+			restTemplate.exchange("http://127.0.0.1:9091/value/test", HttpMethod.POST, requestRest, String.class);
+			tracer.close(span);
+		} catch (Exception e) {
+			System.out.println("Error"+ e.getMessage());
+		}
+		return response;
+	}
+	
+//	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+//	@ResponsePayload
+//	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+//		GetCountryResponse response = new GetCountryResponse();
+//		response.setCountry(countryRepository.findCountry(request.getName()));
+//		RestTemplate restTemplate = new RestTemplate();
+//		HttpHeaders headers = new HttpHeaders();
+//		Span span = this.spanAccessor.getCurrentSpan();
+//		headers.add(Span.SPAN_NAME_NAME, span.getName() );
+//		headers.add(Span.TRACE_ID_NAME, Span.idToHex(span.getTraceId()));
+//		headers.add(Span.SPAN_ID_NAME, Span.idToHex(span.getSpanId()));
+//		headers.setContentType(MediaType.APPLICATION_JSON);
+//		HttpEntity<String> requestRest = new HttpEntity<>("{}", headers);
+//		restTemplate.exchange("http://127.0.0.1:9091/value/test", HttpMethod.POST, requestRest, String.class);
+//		//restTemplate.postForObject("http://127.0.0.1:9091/value/test", "", String.class);
+//		return response;
+//	}	
+
+}
---
+++ src/main/java/com/example/soap/springboot/soapSpringboot/SoapSpringbootApplication.java
@@ -0,0 +1,18 @@
+package com.example.soap.springboot.soapSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SoapSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapSpringbootApplication.class, args);
+	}
+	@Bean
+	public AlwaysSampler defaultSampler() {
+	  return new AlwaysSampler();
+	}
+}
