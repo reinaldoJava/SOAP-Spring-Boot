bf76d12eb1753b0386bfba2b3d8ee52dd050f28a
Reinaldo Santos <reinaldo.java@gmail.com>
2018-02-14 23:17:40 -0200

    correcao

--- src/main/java/com/example/soap/springboot/soapSpringboot/CountryEndpoint.java
+++ src/main/java/com/example/soap/springboot/soapSpringboot/CountryEndpoint.java
@@ -46,8 +46,7 @@ public class CountryEndpoint {
 			headers.setContentType(MediaType.APPLICATION_JSON);
 			
 			HttpEntity<String> requestRest = new HttpEntity<>("{}", headers);
-			//Comentado para outros testes
-			//restTemplate.exchange("http://127.0.0.1:9091/value/test", HttpMethod.POST, requestRest, String.class);
+			restTemplate.exchange("http://127.0.0.1:9091/value/test", HttpMethod.POST, requestRest, String.class);
 			tracer.close(span);
 		} catch (Exception e) {
 			System.out.println("Error"+ e.getMessage());
